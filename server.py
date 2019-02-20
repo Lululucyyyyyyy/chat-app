65a80079d381f2980dfa5c067e97c7ef13bc33b5
Lucy <lucy13939@gmail.com>
2019-02-19 20:21:12 -0800

    Updates?

--- server.py
+++ server.py
@@ -5,13 +5,33 @@ import sqlite3
 
 app = Flask(__name__)
 
+conn = sqlite3.connect('chat.db')
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS chat (id INTEGER PRIMARY KEY AUTOINCREMENT, my_text text, user INTEGER''');
+conn.commit()
+conn.close()
+
+#this user value will be 1
+
 @app.route('/', methods=["POST", "GET"])
 def homepg():
+	if request.method == 'POST':
+		conn = sqlite3.connect('twitter.db')
+		c = conn.cursor()
+		my_text = request.form['text']
+		c.execute("INSERT INTO chat (my_text, user) values (?, ?)", (my_text, 1))
+		conn.commit()
+		conn.close()
+
+	if request.method == 'GET':
+		get_messages();
 	return render_template('index.html')
 
-@app.route('/hi')
-def hi():
-	return "HI"
+def get_messages():
+	conn = sqlite3.connect('twitter.db')
+	c = conn.cursor()
+	latest_text = c.execute('SELECT * FROM chat'.format(userr))[:15].fetchall()
+	conn.close()
 
 if __name__ == '__main__':
 	app.run()
--- static/chat.js
+++ static/chat.js
@@ -1,21 +1,27 @@
 $( document ).ready(function() {
     $("#send-message").click(function() {
       alert("Handler for .click() called.");
-      /*
+
+    $("#get_message").click(function()){
+      $.fetchAllMessages('chat-app.db', function(data) {
+        $.each(data, function(text, text) {
+          $("#" + text).val(text);
+          console.log(data)
+        });
+      });
+    }
+
+    $.setInterval(fetchAllMessages(), 1000);
+});
+
+          /*
       $.get( "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty", function( data ) {
-  		var top_10 = data.slice(0, 10);
-  		for (var i = 0; i < top_10.length; i++){
-  			$.get(`https://hacker-news.firebaseio.com/v0/item/${top_10[i]}.json?print=pretty`, function(data){
-  				var title = data['title'];
-  				$( "#random-string" ).append( `<p>${title}</p>` );
-  			});
-  		}
+      var top_10 = data.slice(0, 10);
+      for (var i = 0; i < top_10.length; i++){
+        $.get(`https://hacker-news.firebaseio.com/v0/item/${top_10[i]}.json?print=pretty`, function(data){
+          var title = data['title'];
+          $( "#random-string" ).append( `<p>${title}</p>` );
+        });
+      }
       });
       */
-    $.fetchAllMessages('chat-app.db', function(data) {
-    $.each(data, function(text, text) {
-      $("#" + text).val(text);
-    });
-    setInterval(fetchAllMessages(), 1000);
-    });
-});
